12b14ed4acbeb66150ddfbe4fef8c5f838440602
Rahul Mirashi <rpmirashi@gmail.com>
2024-08-03 23:07:33 +0530

    Added xgboost in requirements.txt and updated the GitHib link in Get Code button

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from predict_page import show_predict_page
 from explore_page import show_explore_page
 
 page = st.sidebar.selectbox("Explore or Predict",{"Predict","Explore"})
-st.sidebar.link_button("Get Code", "https://www.linkedin.com/in/rahul-mirashi/")
+st.sidebar.link_button("Get Code", "https://github.com/RahulMirashi/Health-insurance-price-prediction")
 if page == "Predict":
     show_predict_page() 
 else:
